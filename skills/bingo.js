870730113cc0e1121430622f450acf18ff3e4970
santosomar <santosomar@gmail.com>
2018-03-12 17:59:15 -0400

    update

--- skills/bingo.js
+++ skills/bingo.js
@@ -5,16 +5,9 @@ module.exports = function (controller) {
 
     controller.hears([/^bingo$/], 'direct_message,direct_mention', function (bot, message) {
         var text = "Here is your corporate BS bingo card:";
-        text = "this may not work";
-        text += "\n- " + bot.appendMention(message, "bs") + ": Generates random Corporate BS statement";
-        text += "\n- " + bot.appendMention(message, "restricted") + ": let a user pick a color among a set of options";
-        text += "\n- " + bot.appendMention(message, "storage") + ": store picked color as a user preference";
-        text += "\n- " + bot.appendMention(message, "threads") + ": branch to another thread";
-        text += "\n- " + bot.appendMention(message, "variables") + ": enriched user-context among threads";
-        text += "\n\nI also understand:";
-        text += "\n- " + bot.appendMention(message, "about") + ": shows metadata about myself";
-        text += "\n- " + bot.appendMention(message, "help") + ": spreads the word about my skills";
-        text += "\n- " + bot.appendMention(message, "show [skill]") + ": display the code of the specified skill";
+        text += "\n- " + "this may not work";
+        text += "\n- " + "Generates random Corporate BS statement";
+
         bot.reply(message, text);
     });
 }
