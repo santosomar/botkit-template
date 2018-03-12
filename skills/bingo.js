1f20fe0decd02f5c0fdf7b688244770abde57a4d
santosomar <santosomar@gmail.com>
2018-03-12 18:36:59 -0400

    update

--- skills/bingo.js
+++ skills/bingo.js
@@ -5,16 +5,7 @@ module.exports = function (controller) {
 
     controller.hears([/^bingo$/], 'direct_message,direct_mention', function (bot, message) {
       //note this may be async
-     fs = require('fs')
-     fs.readFile('words.txt', 'utf8', function (err,data) {
-       if (err) {
-         return console.log(err);
-       }
-       data = data.split('\n');
-
-
-
-
+       var data = "testing";
         var text = "C O R P   B S   B I N G O ! ! !";
         text += "\n- " + "========================";
         text += "\n- " + data ;
