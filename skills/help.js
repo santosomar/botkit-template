0d2ecda33afdf823f184089edf3373edbfa8e5c9
santosomar <santosomar@gmail.com>
2018-03-12 17:47:09 -0400

    updating

--- skills/help.js
+++ skills/help.js
@@ -4,17 +4,10 @@
 module.exports = function (controller) {
 
     controller.hears([/^help$/], 'direct_message,direct_mention', function (bot, message) {
-        var text = "Here are my skills:";
-        text += "\n- " + bot.appendMention(message, "color") + ": this may not work";
-        text += "\n- " + bot.appendMention(message, "bs") + ": Generates random Corporate BS statement";
-        text += "\n- " + bot.appendMention(message, "restricted") + ": let a user pick a color among a set of options";
-        text += "\n- " + bot.appendMention(message, "storage") + ": store picked color as a user preference";
-        text += "\n- " + bot.appendMention(message, "threads") + ": branch to another thread";
-        text += "\n- " + bot.appendMention(message, "variables") + ": enriched user-context among threads";
-        text += "\n\nI also understand:";
-        text += "\n- " + bot.appendMention(message, "about") + ": shows metadata about myself";
-        text += "\n- " + bot.appendMention(message, "help") + ": spreads the word about my skills";
-        text += "\n- " + bot.appendMention(message, "show [skill]") + ": display the code of the specified skill";
+        var text = "Don't abuse me... Here are my skills:";
+        text += "\n- " + bot.appendMention(message, "bingo") + ": this will generate a random corporate BS bingo card";
+        text += "\n- " + bot.appendMention(message, "bs") + ": (coming soon) Generates random Corporate BS statement";
+        text += "\n- " + bot.appendMention(message, "help") + ": you are already here dumbass!";
         bot.reply(message, text);
     });
 }
