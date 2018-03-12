70882ffc5fc8d32d89b82a1c86205ed5f7df09b5
santosomar <santosomar@gmail.com>
2018-03-12 17:37:40 -0400

    trying this out

--- skills/bs.js
+++ skills/bs.js
@@ -4,7 +4,8 @@ module.exports = function (controller) {
     controller.hears([/^bs$/], 'direct_message,direct_mention', function (bot, message) {
 
         bot.startConversation(message, function (err, convo) {
-            convo.say('This is awesome...');
+            convo.say('Hi <@personId:${event.actorId}>, here is your bingo card');
+            convo.say('Hi <@personId:${event.actorId}>, here is your bingo card');
 
             convo.ask('How about this??', function (response, convo) {
                 convo.say("Did you like it? '" + response.text + "' too!");
