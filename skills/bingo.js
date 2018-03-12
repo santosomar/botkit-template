9d525a2a1b89a0ba2faf0fde631a532ff8f487a9
santosomar <santosomar@gmail.com>
2018-03-12 18:32:45 -0400

    update

--- skills/bingo.js
+++ skills/bingo.js
@@ -1,13 +1,27 @@
 //
-// Command: help
+// Command: bingo - this is the corporate bullshit bingo card generator
 //
 module.exports = function (controller) {
 
     controller.hears([/^bingo$/], 'direct_message,direct_mention', function (bot, message) {
-        var text = "Here is your corporate BS bingo card:";
-        text += "\n- " + "this may not work";
-        text += "\n- " + "Generates random Corporate BS statement";
+      //note this may be async
+     fs = require('fs')
+     fs.readFile('words.txt', 'utf8', function (err,data) {
+       if (err) {
+         return console.log(err);
+       }
+       data = data.split('\n');
+
+
+
+
+        var text = "C O R P   B S   B I N G O ! ! !";
+        text += "\n- " + "========================";
+        text += "\n- " + data ;
 
         bot.reply(message, text);
+
+     });
+
     });
 }
